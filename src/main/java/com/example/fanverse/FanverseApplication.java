908b7eae4b6cc5f27c0bd3b8312ca1f712ebebcc
poogeun <jiy2797@naver.com>
2025-10-27 17:00:04 +0900

    initial project setup

---
+++ src/main/java/com/example/fanverse/FanverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.fanverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fanverse/FanverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fanverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
